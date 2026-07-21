e3d46cb424f83baff1b40e16f97174890fc54fd8
Jendres73 <joejhosep7300@gmail.com>
2026-07-21 16:25:00 -0500

    0.1

--- Assets/ContenidoJuego/CodigoUnoMAs/EnemyUnit.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-// Solo con poner este script en el objeto, el Manager sabrá que es un enemigo
-public class EnemyUnit : MonoBehaviour
-{
-    // Opcional: Puedes dejarlo vacío
-}
